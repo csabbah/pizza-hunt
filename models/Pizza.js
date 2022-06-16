b9f5bfcebf64a537b337bf6d1c19e0a952311d6c
Csabbah <csabbah@ryerson.ca>
2022-06-16 15:32:45 -0400

    Added validation to any data related to the Pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -6,14 +6,20 @@ const PizzaSchema = new Schema(
     pizzaName: {
       type: String, // Rather than using 'data.STRING', we can use regular
       // JavaScript data types like String, Booleans, numbers and so on...
+      required: true, // pizzaName is required
+      // We can als specify a specific message like so: required: 'You need to provide a pizza name!',
+      trim: true, // Acts like regular JS, removes white spaces
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
     },
     size: {
       type: String,
+      required: true,
+      // enum ensures that the below options must be chosen for this data to be valid otherwise, reject it
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large',
     },
     toppings: [],
